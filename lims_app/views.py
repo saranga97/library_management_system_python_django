fa04b68b5375ec61a39fde48cd6250be834cba5c
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-04-30 20:53:26 +0530

    first view created

--- lims_app/views.py
+++ lims_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib import admin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Hello world. You're at the home page.")
